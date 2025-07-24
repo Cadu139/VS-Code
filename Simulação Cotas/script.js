f9aedfc8dee83a9ebd1bddc9179d90dfacaed20b
Cadu139 <cadu.227567@gmail.com>
2025-07-24 12:56:19 -0300

    24/07/2025 - 12:54

--- Simulação Cotas/script.js
+++ Simulação Cotas/script.js
@@ -1,51 +1,83 @@
 
-const layers = [];
-const nlayers = 2; // Number of layers to be used
+//#region : Cria as camadas dinamicamente
 
-for (let z = 0; z <= nlayers-1; z++){
-    layers.push(document.getElementById(`layer${z}`));
-    document.getElementById(`layer${z}`).style.zIndex = z;
-}
+    const layers = []; // Array para armazenar as camadas
+    const nlayers = 5; // Número de camadas a serem criadas
 
-const canvas = layers[1];
-
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
-
-const ctx = canvas.getContext('2d');
-
-const totalRow = 20;
-const TILE_SIZE = (canvas.width) / totalRow;
-const INTERVAL = 250;
-
-function draw(x, y, color, count_x, count_y) {
-    ctx.fillStyle = color;
-    for (let cX = 0; cX < count_x; cX++) {
-        for (let cY = 0; cY < count_y; cY++) {
-            ctx.fillRect((TILE_SIZE + 0.125) * (x + cX - 1), (TILE_SIZE + 0.125) * (y + cY - 1), (TILE_SIZE - 2.5), (TILE_SIZE - 2.5));
-        };
-    };
-}
-
-function drawGrade(step, count, interval) {
-    for (let d = step; d < (step + count); d++) {
-        setTimeout(() => {
-            for (let a = 1; a <= 20; a++){
-                for (let b = 1; b <= 20; b++){
-                    if (a + b == d + 1 ){
-                        draw(a, b, 'lightgreen', 1, 1);
-                    }
-                }
-            }
-        }, interval * d);
+    for (let z = 0; z <= nlayers-1; z++){
+        layers.push(document.getElementById(`${z}`));
+        document.getElementById(`${z}`).style.zIndex = z;
     }
-};
 
-canvas.addEventListener('click', function() {drawGrade(1, totalRow * 2, 200)});
+//#endregion
 
-canvas.addEventListener('mousemove', function(mouse) {
-    const xmouse = (mouse.pageX - canvas.offsetLeft) * (totalRow / canvas.width);
-    const ymouse = (mouse.pageY - canvas.offsetTop) * (totalRow / canvas.height);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    draw(xmouse, ymouse, "red", 1, 1);
-});
+//#region : Variáveis de configuração do canvas
+
+    const canvas = (z) => layers[z];
+    canvas(0).width = canvas(0).clientWidth;
+    canvas(0).height = canvas(0).clientHeight;
+    canvas(1).width = canvas(1).clientWidth;
+    canvas(1).height = canvas(1).clientHeight;
+    const ctx = (z) => layers[z].getContext('2d');
+
+//#endregion
+
+//#region : Variáveis de configuração do grid
+
+    const tileSize = 10; // Tamanho de cada quadrado do grid
+    const interval = 0.5 ** tileSize; // Intervalo de tempo entre os passos do grid
+    const totalLine = canvas(0).width / tileSize; // Tamanho das linhas e colunas do grid
+
+//#endregion
+
+//#region : Desenho quadrado
+
+    function drawSquare(z, x, y, color, count_x, count_y) {
+        ctx(z).fillStyle = color;
+        for (let cX = 0; cX < count_x; cX++) {
+            for (let cY = 0; cY < count_y; cY++) {
+                ctx(z).fillRect((tileSize) * (x + cX - 1), (tileSize) * (y + cY - 1), (tileSize), (tileSize));
+            };
+        };
+    }
+
+//#endregion
+
+//#region : Desenha a animação do grid
+
+    function drawGrid(start, end, mode, r, g, b) {
+        ctx(0).clearRect(0, 0, canvas(0).width, canvas(0).height); // Limpa o canvas
+        if (mode == "homo") {
+            end *= 2; // Se o modo for homogêneo, dobra a contagem de quadrados para todos
+            total = start + end;
+        }
+        else if (mode == "hetero") total = start + end*2; // Se o modo for heterogêneo, duplica a contagem de quadrados para a diagonal
+        for (let x = start; x <= end; x++){
+            for (let y = start; y <= end; y++){
+                drawSquare(0, x, y, `rgba(${r*x}, ${g*y}, ${b*Math.sqrt(x**2 + y**2)}, 1)`, 1, 1); // Define a cor padrão se não for fornecida
+            }
+        }
+    };
+
+//#endregion
+
+//#region : Evento de chamada do grid
+
+    canvas(nlayers-1).addEventListener('click', () => {drawGrid(0, totalLine, "hetero", 1, 1, 1)});
+
+//#endregion
+
+//#region : Cria o player do mouse e atualiza o canvas (player e grid)
+
+    canvas(nlayers-1).addEventListener('mousemove', (mouse) => {
+        const rect = canvas(nlayers-1).getBoundingClientRect();
+        const xmouse = (mouse.pageX - rect.left) * totalLine / canvas(1).width;
+        const ymouse = (mouse.pageY - rect.top) * totalLine / canvas(1).height;
+        ctx(1).clearRect(0, 0, canvas(1).width, canvas(1).height); // Limpa o canvas
+        drawSquare(1, xmouse, ymouse, "yellow", 1, 1);
+
+        
+        drawGrid(0, totalLine, "hetero", (xmouse/Math.sqrt(xmouse**2 + ymouse**2)) * 255, (ymouse/Math.sqrt(xmouse**2 + ymouse**2)) * 255, (Math.sqrt(xmouse**2 + ymouse**2) / Math.sqrt(2) * totalLine) * 255);
+    });
+
+//#endregion
